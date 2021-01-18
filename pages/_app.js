a0241afc1aa7c4ea89d766f0c245c2942f4bc0cb
Lucas Felix <lucasfelixdev@gmail.com>
2021-01-18 12:06:20 -0300

    (feat): Estruturação inicial do projeto

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.any.isRequired,
+  pageProps: PropTypes.any.isRequired
 }
 
 export default MyApp

02c5c85b8f5907ab5e20c265eb9684c336338303
Lucas Felix <lucasfelixdev@gmail.com>
2021-01-18 21:37:20 -0300

    (feat): Configuração inicial das imagens no next, início da estruturação do layout

--- next.config.js
+++ next.config.js
@@ -1,41 +1,35 @@
-// const withLess = require('@zeit/next-less')
-
-// module.exports = withLess({
-//   cssModules: true,
-//   webpack: function (config) {
-//     return config;
-//   },
-//   css: [
-//     { loader: "less-loader", options: { lessOptions: { javascriptEnabled: true } } }
-//   ]
-// });
-
-// next.config.js
+const withPlugins = require('next-compose-plugins')
 const withAntdLess = require('next-plugin-antd-less')
+// const withImages = require('next-images')
 
-module.exports = withAntdLess({
-  lessVarsFilePath: './src/styles/variables.less',
-  cssLoaderOptions: {
-  //   https://github.com/webpack-contrib/css-loader#object
-  //
-  //   sourceMap: true, // default false
-  //   esModule: false, // default false
-  //   modules: {
-  //     exportLocalsConvention: 'asIs',
-  //     exportOnlyLocals: true,
-  //     mode: 'pure',
-  //     getLocalIdent: [Function: getCssModuleLocalIdent]
-  //   }
-  },
-  // Other Config Here...
+module.exports = withPlugins([
+  [withAntdLess, {
+    lessVarsFilePath: './src/styles/variables.less',
+    cssLoaderOptions: {
+    //   https://github.com/webpack-contrib/css-loader#object
+    //
+    //   sourceMap: true, // default false
+    //   esModule: false, // default false
+    //   modules: {
+    //     exportLocalsConvention: 'asIs',
+    //     exportOnlyLocals: true,
+    //     mode: 'pure',
+    //     getLocalIdent: [Function: getCssModuleLocalIdent]
+    //   }
+    },
+    // Other Config Here...
 
-  webpack(config) {
-    config.module.rules.push(
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack']
-      }
-    )
-    return config
-  },
-});
+    webpack(config) {
+      config.module.rules.push(
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack']
+        }
+      )
+      return config
+    },
+  }],
+  // [withImages, {
+  //   esModule: true
+  // }]
+]);
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,75 +1,59 @@
 import 'antd/dist/antd.less'
-// import '../styles/pages/home.less'
-import { Layout, Menu, Breadcrumb } from 'antd'
-import {
-  UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined
-} from '@ant-design/icons'
+import '../styles/pages/home.less'
+import { Layout, List, Card } from 'antd'
 
 import Logo from '../assets/icons/Logo.svg'
+import LogoMin from '../assets/icons/LogoMin.svg'
+import Plus from '../assets/icons/plus.svg'
+// import naruto from '../assets/images/naruto.png'
 
-const { SubMenu } = Menu
-const { Header, Content, Sider } = Layout
+const { Header, Content } = Layout
+
+const data = [
+  {
+    title: 'Title 1'
+  },
+  {
+    title: 'Title 2'
+  },
+  {
+    title: 'Title 3'
+  },
+  {
+    title: 'Title 4'
+  }
+]
 
 export default function Home() {
   return (
     <div>
       <main>
-        <Layout>
+        <Layout className="container">
           <Header className="header">
-            <Logo />
+            <Logo className="logoMax" />
+            <LogoMin className="logoMin" />
           </Header>
-          <Layout>
-            <Sider width={200} className="site-layout-background">
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%', borderRight: 0 }}
-              >
-                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                  <Menu.Item key="1">option1</Menu.Item>
-                  <Menu.Item key="2">option2</Menu.Item>
-                  <Menu.Item key="3">option3</Menu.Item>
-                  <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                  <Menu.Item key="5">option5</Menu.Item>
-                  <Menu.Item key="6">option6</Menu.Item>
-                  <Menu.Item key="7">option7</Menu.Item>
-                  <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub3"
-                  icon={<NotificationOutlined />}
-                  title="subnav 3"
-                >
-                  <Menu.Item key="9">option9</Menu.Item>
-                  <Menu.Item key="10">option10</Menu.Item>
-                  <Menu.Item key="11">option11</Menu.Item>
-                  <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
-              </Menu>
-            </Sider>
-            <Layout style={{ padding: '0 24px 24px' }}>
-              <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                <Breadcrumb.Item>List</Breadcrumb.Item>
-                <Breadcrumb.Item>App</Breadcrumb.Item>
-              </Breadcrumb>
-              <Content
-                className="site-layout-background"
-                style={{
-                  padding: 24,
-                  margin: 0,
-                  minHeight: 280
-                }}
-              >
-                Content
-              </Content>
-            </Layout>
-          </Layout>
+          {/* <img src={naruto} alt="" /> */}
+          <Content className="site-layout-background">
+            <div className="contentImg">
+              <div className="test"></div>
+              <div className="infos">
+                <strong>Name of anime</strong>
+                <button className="btnPlus">
+                  <Plus />
+                </button>
+              </div>
+            </div>
+          </Content>
+          <List
+            grid={{ gutter: 16, column: 4 }}
+            dataSource={data}
+            renderItem={item => (
+              <List.Item>
+                <Card title={item.title}>Card content</Card>
+              </List.Item>
+            )}
+          />
         </Layout>
       </main>
     </div>

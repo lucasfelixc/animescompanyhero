43987d780cb3c1559666ddc140475fcbe95203f5
Lucas Felix <lucasfelixdev@gmail.com>
2021-01-19 17:45:49 -0300

    (fix): Início de configuração do Less para Next.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,40 @@
 const withPlugins = require('next-compose-plugins')
 const withAntdLess = require('next-plugin-antd-less')
+const lessToJS = require('less-vars-to-js');
+const fs = require('fs');
+const path = require('path');
+
+// Where your antd-custom.less file lives
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './assets/styles/variables.less'), 'utf8')
+);
 
 const nextConfig = {
   images: {
     domains: ['media.kitsu.io']
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals)
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader'
+      });
+    }
+
     config.module.rules.push(
       {
         test: /\.svg$/,
@@ -18,7 +47,13 @@ const nextConfig = {
 
 module.exports = withPlugins([
   [withAntdLess, {
-    lessVarsFilePath: './src/styles/variables.less',
-    cssLoaderOptions: {}
-  }]
-], nextConfig);
+    lessLoaderOptions: {
+      javascriptEnabled: true,
+      modifyVars: themeVariables, // make your antd custom effective
+      importLoaders: 0
+    },
+    cssLoaderOptions: {
+      importLoaders: 3,
+      localIdentName: '[local]___[hash:base64:5]',
+    }
+  }], nextConfig]);
